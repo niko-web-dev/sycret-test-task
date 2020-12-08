39a5e354fe10feb1585be2cec44b3f4ef4426e75
Niko o <kolyabokov41@gmail.com>
2020-12-08 20:03:09 +0300

    refactor

--- src/components/NewsPage.jsx
+++ src/components/NewsPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './news.scss'
 
-import { useParams, Link, useHistory } from "react-router-dom"
+import { useParams, useHistory } from "react-router-dom"
 
 const NewsPage = ({news}) => {
     const history = useHistory();
@@ -15,9 +15,7 @@ const NewsPage = ({news}) => {
                 <h2>{newsItem.title}</h2>
                 <img src={newsItem.urlToImage} alt=""/>
                 <p>{newsItem.description}</p>
-                {/*<Link to={"/"}>*/}
                     <button onClick={() => history.goBack()}>Назад</button>
-                {/*</Link>*/}
             </div>
         </div>
     );
